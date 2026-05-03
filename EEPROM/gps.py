91ff7c055110c8684f6bed644e099f8f77a50f90
robotmad <cbarnes@tmsl.com>
2026-05-03 12:07:36 +0100

    tiny GPS app to fit on 2K EEPROM

--- EEPROM/gps.py
+++ EEPROM/gps.py
@@ -1,190 +1,90 @@
-""" GPS App for Hexpansion """
-# Version of the L80K GPS app modified to work direct from EEPROM. (although you do need a larger EEPROM than that on the first production batch of hexpansions).
-# See https://github.com/TechCabin/EMFBadge-Hexpansions-GPS for original code by TechCabin, and the README there for instructions on how to use this app.
-
+# Minimal length method names to make the mpy file as small as possible so it will fit in the 2k hexpansion EEPROM.
+# Minimal functionality to get a GPS fix and display it
+#
 import app
-from app_components.tokens import label_font_size, button_labels
 from events.input import Buttons, BUTTON_TYPES, ButtonDownEvent
 from system.eventbus import eventbus
-from system.hexpansion.config import HexpansionConfig
-from system.patterndisplay.events import PatternDisable, PatternEnable
 from system.scheduler.events import RequestForegroundPopEvent, RequestForegroundPushEvent, RequestStopAppEvent
-#from tildagonos import tildagonos
 from machine import UART, Pin
-
-# Minimal length method names to make the mpy file as small as possible so it might fit in the 2k hexpansion EEPROM.
-# Minimal functionality to get a GPS fix
-# This version is NOT for the App Store
-
-
-
-# Hardware defintions:
-TX_PIN  = 1    # HS_G for TX
-RX_PIN  = 0    # HS_F for RX
-RESET_PIN = 2  # HS_H for reset
-#PPS_PIN = 3    # HS_I for PPS
-
-###JUST FOR USE WITH MY PROTOTYPE BOARD
-#ENABLE_PIN  = 0  # First LS pin used to enable the SMPSU
-###JUST FOR USE WITH MY PROTOTYPE BOARD
 
 class GPSApp(app.App):         # pylint: disable=no-member
     """ App to get GPS data from a GPS module connected to the hexpansion and display it on the badge. """
-    def __init__(self, config: HexpansionConfig | None = None):
+    VERSION = 1             # Increment this when making changes to the app that require the hexpansion app to be re-flashed with the new code.
+
+    def __init__(self, config=None):
         super().__init__()
 
-        self.VERSION = 1         # Increment this when making changes to the app that require the hexpansion app to be re-flashed with the new code.
+        if config is None:
+            raise ValueError("GPSApp config")
 
-        # If run from EEPROM on the hexpansion, the config will be passed in with the correct pin objects
-        self.config: HexpansionConfig | None = config
-        #if config is None:
-        #    return
-        self.tx_pin = config.pin[TX_PIN]
-        self.rx_pin = config.pin[RX_PIN]
-        self.reset = config.pin[RESET_PIN]
-        #self.pps = config.pin[PPS_PIN]
+        self.config = config    # Enables HexManager to check which port app is associated with
+        self.t = config.pin[1]
+        self.x = config.pin[0]
+        self.r = config.pin[2]
+        self.f = False          # Whether the app is in the foreground or not, used to control when to read from the GPS and when to reset it.
+        self.b = Buttons(self)
+        self.l = None           # Last GPS fix as a string in the format "lat,lon" or None if no fix yet. Latitude and longitude are rounded to 5 decimal places which gives a precision of about 1 meter, more than enough for badgebot's purposes.
+        self.u = UART(1, baudrate=9600, tx=self.t, rx=self.x)
+        self.r.init(mode=Pin.OUT)
+        self.r.value(1)
+        self.z = 0              # Ticks since GPS reset, used to control when to release the GPS from reset after resetting it when the app is in the foreground and to prevent reading from the GPS for a short time after resetting it.
+        self.y = 0              # Ticks since last GPS fix, used to control when to discard the last GPS fix after not getting a new one for a while.
 
-###JUST FOR USE WITH MY PROTOTYPE BOARD
-        #self.power_control = config.ls_pin[ENABLE_PIN]
-        #self.power_control.init(mode=Pin.OUT)
-        #self.power_control.value(1)
-###JUST FOR USE WITH MY PROTOTYPE BOARD
-
-        self.foreground = False
-        self.button_states = Buttons(self)
-        self.last_fix = None
-
-        # Event handlers for gaining and losing focus and for stopping the app
-        #eventbus.on_async(RequestStopAppEvent, self.s, self)
-        #eventbus.on_async(RequestForegroundPushEvent, self.r, self)
-        #eventbus.on_async(RequestForegroundPopEvent, self.p, self)
-
-        self.uart = UART(1, baudrate=9600, tx=self.tx_pin, rx=self.rx_pin)
-        self.reset.init(mode=Pin.OUT)
-        #self.pps.init(mode=Pin.IN)
-        self.reset.value(1) # set reset high here and release when 100ms has passed in foreground update.
-        self.ticks_since_start = 0
-        self.ticks_since_last_fix = 0
+        eventbus.on_async(RequestStopAppEvent, self.s, self)
 
 
-    #def deinit(self):
-    #    """ Deinitialise the app, releasing any resources (e.g. UART) """
-    #    self.uart.deinit()
-    #    #self.power_control.value(0)  # Cut power to the GPS to save power when not in use
+    async def s(self, _e: RequestStopAppEvent):
+        """ handle app stop """
+        if _e.app == self:
+            print("s")
+            self.r.value(1)
+            self.u.deinit()
 
 
-    #async def s(self, event: RequestStopAppEvent):
-    #    """ Handle the RequestStopAppEvent so that we can release resources """
-    #    if event.app == self:
-    #        self.deinit()
-
-
-    async def r(self, event: RequestForegroundPushEvent):
-    #    """ Handle the RequestForegroundPushEvent to know when we gain focus """
-        if event.app == self:
-            #eventbus.emit(PatternDisable())
-            eventbus.on(ButtonDownEvent, self.d, self)
-            self.foreground = True
-
-
-    #async def p(self, event: RequestForegroundPopEvent):
-    #    """ Handle the RequestForegroundPopEvent to know when we lose focus """
-    #    if event.app == self:
-    #        #eventbus.emit(PatternEnable())
-    #        eventbus.remove(ButtonDownEvent, self.d, self)
-
-
-    def d(self, event: ButtonDownEvent):
-        """ Handle button down events """
-        if event.button == BUTTON_TYPES["CANCEL"]:
-            self.button_states.clear()
+    def update(self, _d):
+        """ Update the app state - expire last_fix if it is too old """
+        if self.b.get(BUTTON_TYPES["CANCEL"]):
+            print("c")
+            self.b.clear()
             self.minimise()
 
-
-    def update(self, delta):
-        """ Update the app state - expire last_fix if it is too old """
-        if self.reset.value():
-            self.ticks_since_start += delta
-            if self.ticks_since_start > 100:
-                # Release reset after 100ms to allow the GPS to start up
-                self.reset.value(0)
-        if not self.foreground:
-            # This triggers the automatic foreground display
+        if self.r.value():
+            self.z +=_d
+            if self.z > 100:
+                self.r.value(0)
+        if not self.f:
+            print("f")
             eventbus.emit(RequestForegroundPushEvent(self))
-            self.foreground = True
-        #if self.last_fix:
-        #    self.ticks_since_last_fix += delta
-        #    if self.ticks_since_last_fix > 10000:
-        #        # If it's been more than 10 seconds since the last fix, disccard it
-        #        self.last_fix = None
+            self.f = True
+        if self.l:
+            self.y += _d
+            if self.y > 10000:
+                self.l = None
 
 
-    def background_update(self, _delta):
-        """ Update in the background - read from the UART and parse any GPS data """
-        line = self.uart.readline()
-        if line:
+    def background_update(self, _d):
+        """ Update the app state in the background - read GPS data """
+        l = self.u.readline()
+        if l:
             try:
-                line = line.decode().strip()
-                result = n(line)
-                if result:
-                    self.last_fix = result
-                    self.ticks_since_last_fix = 0
+                p = l.decode().strip().split(',')
+                if (p[0] != "$GPRMC" and p[0] != "$GNRMC") or p[2] != "A" or not p[3] or not p[5]:
+                    return None
+                t = float(p[3][:2]) + float(p[3][2:]) / 60
+                n = float(p[5][:3]) + float(p[5][3:]) / 60
+                if p[4] == "S":
+                    t = -t
+                if p[6] == "W":
+                    n = -n
+                self.l = str(round(t, 5)) + "," + str(round(n, 5))#
+                self.y = 0
             except (UnicodeError, ValueError, AttributeError):
                 pass
 
 
-    def draw(self, ctx):
-        """ Draw the app - display the last GPS fix or a searching message if no fix is available """
-        ctx.rgb(0, 0.2, 0).rectangle(-120, -120, 240, 240).fill()
-        ctx.rgb(0, 1, 0)
-        #ctx.font_size = label_font_size
-        #ctx.text_align = ctx.LEFT
-        #ctx.text_baseline = ctx.BOTTOM
-        if self.last_fix:
-            ctx.move_to(-100, -10).text("Lat: " + str(round(self.last_fix["lat"], 5)))
-            ctx.move_to(-100, 20).text("Lon: " + str(round(self.last_fix["lon"], 5)))
-            #for i in range(1, 13):
-            #    tildagonos.leds[i] = (0,1,0)
-            #tildagonos.leds.write()
-        #else:
-        #    ctx.move_to(-100, 0).text("Searching...")
-            #for i in range(1,13):
-            #    tildagonos.leds[i] = (0,0,0)
-            #tildagonos.leds.write()
-
-        # show labels for buttons
-        #button_labels(ctx, cancel_label="Exit")
-
-def n(line: str) -> dict[str, float] | None:
-    """ Parse an NMEA RMC sentence and return a dictionary with the latitude and longitude if valid, or None if invalid. """
-    parts = line.split(',')
-
-    if parts[0] not in ("$GNRMC", "$GPRMC"):
-        return None
-    elif parts[2] != "A":  # A = valid, V = invalid
-        return None
-    else:
-        lat_raw = parts[3]
-        lat_dir = parts[4]
-        lon_raw = parts[5]
-        lon_dir = parts[6]
-
-        if not lat_raw or not lon_raw:
-            return None
-
-    # Convert to decimal degrees
-        lat = float(lat_raw[:2]) + float(lat_raw[2:]) / 60
-        lon = float(lon_raw[:3]) + float(lon_raw[3:]) / 60
-
-        if lat_dir == "S":
-            lat = -lat
-        if lon_dir == "W":
-            lon = -lon
-
-        return {
-            "lat": lat,
-            "lon": lon
-        }
+    def draw(self, _c):
+        _c.rgb(0, 0.2, 0).rectangle(-120, -120, 240, 240).fill()
+        _c.rgb(0, 1, 0).move_to(-100, -10).text(self.l if self.l else "Search")
 
 
 __app_export__ = GPSApp     #pylint: disable=invalid-name
